1fa06ca1077f022c971ec2924c267c273433b072
NazarovIlya <eliakohai@gmail.com>
2023-03-25 16:02:43 +0300

    Possible solution the problem. CHECKING FOR NULL

--- DesignPatterns/NullObject_01/Program.cs
+++ DesignPatterns/NullObject_01/Program.cs
@@ -1,1 +1,16 @@
-﻿// Есть некий магазин с товарами //   using NullObject_01;  Console.Clear(); ListProduct listProducts = new ListProduct();  foreach (string productName in listProducts.products) { 	Product product = Shop.GetProductOf(productName); 	Console.WriteLine(product.KCalOfProduct()); 	product.eat(); }  Console.ReadKey();  
+﻿// Есть некий магазин с товарами
+// 
+
+using NullObject_01;
+
+Console.Clear();
+ListProduct listProducts = new ListProduct();
+
+foreach (string productName in listProducts.products)
+{
+	Product product = Shop.GetProductOf(productName);
+	Console.WriteLine(product.KCalOfProduct());
+	product.eat();
+}
+
+Console.ReadKey();
