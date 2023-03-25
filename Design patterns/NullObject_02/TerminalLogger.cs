db7273821a2aee2f4f2e922ca8df2aaf7ac83498
NazarovIlya <eliakohai@gmail.com>
2023-03-25 21:07:18 +0300

    project initialized

---
+++ Design patterns/NullObject_02/TerminalLogger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullObject_02
+{
+	internal class TerminalLogger : ILogger
+	{
+	}
+}
